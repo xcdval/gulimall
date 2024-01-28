5fba53392151d20fa541441fc8f6df7b9a18968e
xcdval <veenxx@qq.com>
2024-01-28 21:33:10 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/com/xcdval/gulimallcoupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xcdval.gulimallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-ware/src/main/java/com/xcdval/gulimall/gulimallware/GulimallwareApplication.java
@@ -0,0 +1,13 @@
+package com.xcdval.gulimall.gulimallware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallwareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallwareApplication.class, args);
+    }
+
+}
